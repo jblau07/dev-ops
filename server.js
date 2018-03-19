cde943d813fe9c1c00a56e10a4ece74d6e68a13c
jblau07 <jlau7248@gmail.com>
2018-03-19 10:13:15 -1000

    boilerplate setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || '4200';
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log('SERVER LISTENING ON: ' + PORT)
+});
+
